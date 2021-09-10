7d00bca248119eec98d5eac518bb8744501021e8
Alexandrt Volkov <Alexandr1990@gitlab.com>
2021-09-10 17:34:50 +0300

    master: убрана зависимость с cms, изменены соответствующие настройки, в зависимости добавлен модул drf-yasg, environs

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -31,10 +31,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'modeltranslation',
-    'polymorphic_tree',
-    'polymorphic',
-    'mptt',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
